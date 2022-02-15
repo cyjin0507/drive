b331cc9f044fcab858fa733e7f19c2d4eadcc494
neneong <cyjin0507@gmail.com>
2022-02-15 11:37:48 +0900

    commit

---
+++ 기능반/특강/B_Module특강/과제/indexedDB 사진 업로드/DropBox.js
@@ -0,0 +1,79 @@
+class DropBox {
+    constructor({ elem }) {
+        this.elem = elem;
+        this.list = [];
+
+        this.init();
+    }
+
+    async init() {
+        this.loadList();
+        await this.setList();
+        this.addEvent();
+    }
+    
+    addEvent() {
+        const { elem } = this;
+
+        // 해당 요소에 드래그를 하고 있는 중일 때
+        elem.addEventListener("dragover", (ev) => this.onDragOver(ev));
+
+        // 해당 요소를 드랍할 때
+        elem.addEventListener("drop", (ev) => this.onDrop(ev));
+        
+        // 해당 요소에서 벗어났을 때
+        elem.addEventListener("dragleave", (ev) => this.onDragLeave(ev));
+    }
+    
+    onDragOver(ev) {
+        ev.preventDefault();
+        this.elem.classList.add("dragover");
+    }
+
+    onDragLeave(ev) {
+        this.elem.classList.remove("dragover");
+    }
+
+    async onDrop(ev) {
+        const { elem, list } = this;
+
+        ev.preventDefault();
+        elem.classList.remove("dragover");
+        const files = ev.dataTransfer.files;
+
+        for (const file of files) {
+            try {
+                if (file.name.match(/(jpeg|jpg|gif|png)/)) {
+                    const base64 = await File.loadImage(file);
+                    list.push(base64);
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        await this.setList();
+        this.saveList();
+    }
+
+    async setList() {
+        const { list, elem } = this;
+
+        elem.innerHtml = ''
+
+        for (const base64 of list) {
+            const img = await Element.createImage(base64);
+            elem.appendChild(img);
+        }
+    }
+
+    loadList() {
+        const images = localStorage.getItem("DropBox_images");
+
+        this.list = images ? JSON.parse(images) : [];
+    }
+
+    saveList() {
+        localStorage.setItem("DropBox_images", JSON.stringify(this.list));
+    }
+}
