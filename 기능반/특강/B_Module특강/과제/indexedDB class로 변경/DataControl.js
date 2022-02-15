f39c1d9d68e925e513626fe3c8ea3286ca3102d0
neneong <cyjin0507@gmail.com>
2022-02-15 10:28:42 +0900

    commit

--- 기능반/특강/B_Module특강/과제/indexedDB class로 변경/DataControl.js
+++ 기능반/특강/B_Module특강/과제/indexedDB class로 변경/DataControl.js
@@ -1,28 +1,46 @@
 export class DataControl {
+  dataAdd(objectStore, name, age) {
+    objectStore.add({
+      name: name,
+      age: age,
+    });
+  }
 
-    dataAdd(objectStore, name, age) {
-        objectStore.add({
-            name : name,
-            age : age
-        });
+  dataRemove(objectStore, id) {
+    objectStore.delete(id);
+  }
+
+  dataSearch(objectStore, id) {
+    const req = objectStore.get(id);
+    req.onsuccess = () => {
+      console.log(req.result);
+    };
+  }
+
+  dataModify(objectStore, id, name, age) {
+    objectStore.put({
+      idx: id,
+      name: name,
+      age: age,
+    });
+  }
+
+  dataAllSearch1(objectStore) {
+    const req = objectStore.getAll();
+    req.onsuccess = function () {
+      console.log(req.result);
+    };
+  }
+
+  dataAllSearch2(objectStore) {
+    const cur = objectStore.openCursor();
+    cur.onsuccess = (ev) => {
+        const cursor = ev.target.result;
+        if(cursor) {
+            cursor.value; // 현재 값
+            console.log(cursor.value);
+            cursor.continue(); // 다음 데이터로 이동
+        }
     }
-
-    dataRemove(id) {
-        objectStore.delete(id);
-    }
-
-    dataSearch() {
-
-    }
-
-    dataModify() {
-
-    }
-
-    dataAllSearch() {
-
-    }
-
-
-
+  }
 }
--- 기능반/특강/B_Module특강/과제/indexedDB class로 변경/OpenDB.js
+++ 기능반/특강/B_Module특강/과제/indexedDB class로 변경/OpenDB.js
@@ -11,43 +11,66 @@ export class openDB {
             console.log("DB 오픈 성공");
             this.db = ev.target.result;
             const transaction = this.db.transaction("users", "readwrite")
-            const objectStore = transaction.objectStore("users");
+            this.objectStore = transaction.objectStore("users");
 
-            new DataControl().dataAdd(objectStore, "홍길동", "23");
-
+            this.dataControl();
         }
         
         
         this.req.onerror = (ev) => {
             console.log("DB 오픈 실패");
         }
-
+        
         this.insertDB();
-
+        
     }
-
+    
     insertDB() {
         this.req.onupgradeneeded = (ev) => {
             console.log("DB최초생성");
             this.db = ev.target.result; // IDBDatabase 객체
-
+            
             const stores = ["users"];
             stores.forEach(storeName => {
-
+                
                 // 기존 객체 저장소가 존재할 경우
                 if (this.db.objectStoreNames.contains("user")) {
                     // 기존 객체 저장소 삭제
                     this.db.deleteObjectStore("users");
                 }
-
+                
                 // 객체 저장소 생성
                 this.db.createObjectStore("users", {
                     keyPath: "idx",
                     autoIncrement: true
                 })
-
+                
             })
         }
     }
+    
+    
+    dataControl() {
+        // 데이터 추가
+        // new DataControl().dataAdd(this.objectStore, "sdsd홍길동", "23");
 
+        //데이터 삭제
+        // new DataControl().dataRemove(this.objectStore, 7);
+        
+        // 데이터 수정
+        // let idx = 8; let name = "아아아"; let age = 110;
+        // new DataControl().dataModify(this.objectStore, idx, name, age);
+
+        //데이터 하나 조회
+        // new DataControl().dataSearch(this.objectStore, 8);
+
+        // 모든 데이터 조회1
+        // new DataControl().dataAllSearch1(this.objectStore);
+
+        // 모든 데이터 조회2
+        // new DataControl().dataAllSearch2(this.objectStore);
+
+        
+    }
+    
 }
